56221d4a78282195b887f5b026aa53e32170d139
Lucas <llucassduarte50@gmail.com>
2021-08-26 18:24:18 -0300

    Tela de cronograma

--- lib/app/views/cronograma_page.dart
+++ lib/app/views/cronograma_page.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:path_provider/path_provider.dart';
 
 class cronogramaPage extends StatefulWidget {
   @override
@@ -7,62 +11,171 @@ class cronogramaPage extends StatefulWidget {
 }
 
 class _cronogramaPageState extends State<cronogramaPage> {
+  final _toDoController = TextEditingController();
+
+  List _toDoList = [];
+
+  late Map<String, dynamic> _lastRemoved;
+  late int _lastRemovedPos;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data!);
+      });
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+
+      _saveData();
+    });
+
+    return null;
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+
+      _saveData();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Cronograma"),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('cronograma').snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
-          switch(snapshot.connectionState){
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center (child: CircularProgressIndicator());
-            default:
-              return ReorderableListView.builder(
-                itemCount: (snapshot.data!).docs.length,
-                onReorder: (oldIndex, newIndex) => setState(() {
-                  final index = newIndex > oldIndex ? newIndex - 1 : newIndex;
-
-                  final user = (snapshot.data!).docs.toList().removeAt(oldIndex);
-                  (snapshot.data!).docs.toList().insert(index, user);
-                }),
-                itemBuilder: (context, index) {
-                  final user = (snapshot.data!).docs.toList()[index];
-
-                  return buildUser(index, user);
-                },
-              );
-          }
-        },
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: TextField(
+                      controller: _toDoController,
+                      decoration: InputDecoration(
+                          labelText: "Novo item",
+                          labelStyle: TextStyle(color: Colors.blueAccent)
+                      ),
+                    )
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                  onPressed: _addToDo,
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: RefreshIndicator(onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: shuffleList,
-      )
     );
   }
 
-  Widget buildUser(int index, user) => ListTile(
-    key: ValueKey(user),
-    contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-    title: Text(user.descricao),
-    trailing: Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        IconButton(
-          icon: Icon(Icons.delete, color: Colors.black),
-          onPressed: () => remove(index),
+  Widget buildItem(BuildContext context, int index) {
+    return Dismissible(
+      key: Key(DateTime
+          .now()
+          .millisecondsSinceEpoch
+          .toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white,),
         ),
-      ],
-    ),
-  );
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ?
+          Icons.check : Icons.error),),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
 
-  void remove(int index) => setState(() => {});
-  void shuffleList() => setState(() => {});
+          //_saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<String?> _readData() async {
+    try {
+      final file = await _getFile();
+
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
 }
